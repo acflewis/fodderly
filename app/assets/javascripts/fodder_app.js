074e01997a825ede5eb211fd0a1450c996cffcb3
acflewis <acflewis@gmail.com>
2013-11-15 14:48:42 -0800

    Debug for Heroku 2

--- app/assets/javascripts/fodder_app.js
+++ app/assets/javascripts/fodder_app.js
@@ -3,26 +3,26 @@ window.fodderApp = {
   Collections: {},
   Views: {},
   Routers: {},
-  // initialize: function() {
-//   var cu = JSON.parse($("#bootstrapped_current_user").html()).current_user;
-//   if (cu===null) {
-//     fodderApp.currentUser = new fodderApp.Models.User;
-//   }  else {
-//     fodderApp.currentUser = new fodderApp.Models.User(cu, {parse:true});
-//   }
-//   fodderApp.recipes = new fodderApp.Collections.Recipes();
-//   var $nb_el = $("#navbar");
-//   fodderApp.navBar = new fodderApp.Views.SessionsListen({el: $nb_el});
-//   fodderApp.navBar.render();
-//   $("#changing-nav").html(fodderApp.navBar.$el)
-//   fodderApp.recipes.fetch({
-//     success: function () {
-//       var appRouter = new fodderApp.AppRouter();
-//       appRouter.setUpSidebar();
-//       Backbone.history.start();
-//     }
-//   });    
-//   }
+  initialize: function() {
+  var cu = JSON.parse($("#bootstrapped_current_user").html()).current_user;
+  if (cu===null) {
+    fodderApp.currentUser = new fodderApp.Models.User;
+  }  else {
+    fodderApp.currentUser = new fodderApp.Models.User(cu, {parse:true});
+  }
+  fodderApp.recipes = new fodderApp.Collections.Recipes();
+  var $nb_el = $("#navbar");
+  fodderApp.navBar = new fodderApp.Views.SessionsListen({el: $nb_el});
+  fodderApp.navBar.render();
+  $("#changing-nav").html(fodderApp.navBar.$el)
+  fodderApp.recipes.fetch({
+    success: function () {
+      var appRouter = new fodderApp.AppRouter();
+      appRouter.setUpSidebar();
+      Backbone.history.start();
+    }
+  });    
+  }
 };
 
 $(document).ready(function(){
